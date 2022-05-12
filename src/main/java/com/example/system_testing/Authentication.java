bf04d68321a7f8681058d4b489b69b10b43480e3
root <averkievnv@gmail.com>
2022-05-13 00:08:43 +0300

    я спать, все работает добавлено несколько переименований

--- src/main/java/com/example/system_testing/Authentication.java
+++ src/main/java/com/example/system_testing/Authentication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class AuthenticationApplication extends Application {
+public class Authentication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AuthenticationApplication.class.getResource("authentication.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Authentication.class.getResource("authentication.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
